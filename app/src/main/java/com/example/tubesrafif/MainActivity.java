704076b35573fb2ace2ba2a0fe563060c456ade8
byf1sh <abynhfzhtq@gmail.com>
2024-06-03 23:13:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tubesrafif/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.tubesrafif;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity{
+    ;
+    private Button login;
+    private TextView signup,forgot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.login);
+        signup = findViewById(R.id.signup);
+        forgot = findViewById(R.id.forgot);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
